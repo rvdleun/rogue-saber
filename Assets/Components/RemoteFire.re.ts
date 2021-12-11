eb7e73cc8700cab965738fb888125fb911d44735
Ruben van der Leun <ruben@leunix.nl>
2021-12-11 17:43:30 +0100

    feat(remote-laser): Setup sound

--- Assets/Components/RemoteFire.re.ts
+++ Assets/Components/RemoteFire.re.ts
@@ -1,24 +1,60 @@
 import * as RE from 'rogue-engine';
 import { Prefab, Prop, Runtime } from 'rogue-engine';
-import { Vector3 } from 'three';
+import { PositionalAudio, Vector3 } from 'three';
 import LightsaberBlade from './LightsaberBlade.re';
 
 const vector = new Vector3();
 export default class RemoteFire extends RE.Component {
+  @Prop("Boolean")
+  alwaysFire: boolean = false;
+
+  @Prop("PositionalAudio")
+  fireSound: PositionalAudio;
+
   @Prop("Prefab")
   laserPrefab: Prefab;
 
+  private currentAudio = 0;
   private nextFire = 1;
+  private fireSounds: PositionalAudio[] = [];
+
+  start() {
+    for(let i = 0; i < 10; i++) {
+      const audio = new PositionalAudio(this.fireSound.listener);
+      audio.setBuffer(this.fireSound.buffer as AudioBuffer);
+
+      console.log(this.fireSound.buffer);
+      this.fireSounds.push(audio);
+      this.object3d.add(audio);
+    }
+  }
 
   update() {
     this.nextFire-=Runtime.deltaTime;
 
-    if (this.nextFire > 0 || !LightsaberBlade.active) {
+    if (this.nextFire > 0) {
       return;
     }
 
-    this.fire();
+    if ((this.alwaysFire || LightsaberBlade.active )) {
+      this.fire();
+    }
+
     this.nextFire = .5 + Math.random() * 2;
+    this.playAudio();
+  }
+
+  playAudio() {
+    console.log(this.nextFire);
+
+    this.currentAudio++;
+    if (this.currentAudio >= this.fireSounds.length) {
+      this.currentAudio = 0;
+    }
+
+    const fireSound = this.fireSounds[this.currentAudio];
+    fireSound.offset = 2 - this.nextFire;
+    fireSound.play();
   }
 
   fire() {
